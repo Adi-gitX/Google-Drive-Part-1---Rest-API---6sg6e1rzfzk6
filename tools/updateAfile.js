eebe242abd5af638118f5bcdbd25b703d8161fa7
adi-gitX <adityakammati.workspace@gmail.com>
2024-08-17 01:35:22 +0530

    Update updateAfile.js

--- tools/updateAfile.js
+++ tools/updateAfile.js
@@ -1,5 +1,13 @@
-const updateAfile = () => {
+import fs from 'fs';
 
+const updateAfile = (fileName, newFileData) => {
+  const filePath = `./root/${fileName}`;
+  if (fs.existsSync(filePath)) {
+    fs.writeFileSync(filePath, newFileData);
+    return { message: 'File updated successfully' };
+  } else {
+    return { message: 'File does not exist' };
+  }
 };
 
-export default updateAfile
+export default updateAfile;
