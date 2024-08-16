0c64d81365bcb8fc7cc36b3a845d7c9a08658f57
adi-gitX <adityakammati.workspace@gmail.com>
2024-08-17 01:34:08 +0530

    Update createAfile.js

--- tools/createAfile.js
+++ tools/createAfile.js
@@ -1,5 +1,9 @@
-const createAfile = (fileName, fileData) => {
+import fs from 'fs';
 
+const createAfile = (fileName, fileData) => {
+  const filePath = `./root/${fileName}`;
+  fs.writeFileSync(filePath, fileData);
+  return { message: 'File created successfully' };
 };
 
 export default createAfile;
