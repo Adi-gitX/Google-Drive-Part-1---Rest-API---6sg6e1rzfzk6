99f896e145a636389e73c2a9d51bbcb3209f9afa
adi-gitX <adityakammati.workspace@gmail.com>
2024-08-17 01:34:48 +0530

    Update getAFile.js

--- tools/getAFile.js
+++ tools/getAFile.js
@@ -1,5 +1,13 @@
-const getAFile = () => {
+import fs from 'fs';
 
-}
+const getAFile = (fileName) => {
+  const filePath = `./root/${fileName}`;
+  if (fs.existsSync(filePath)) {
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    return { fileContent };
+  } else {
+    return { message: 'File does not exist' };
+  }
+};
 
 export default getAFile;
