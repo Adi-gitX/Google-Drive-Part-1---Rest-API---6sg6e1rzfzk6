c399001b8bf7c48582c82c743ea705f7049697a4
adi-gitX <adityakammati.workspace@gmail.com>
2024-08-17 01:35:08 +0530

    Update getAllFilename.js

--- tools/getAllFilename.js
+++ tools/getAllFilename.js
@@ -1,5 +1,12 @@
-const getAllFiles = () => {
+import fs from 'fs';
 
+const getAllFiles = (dirPath) => {
+  if (fs.existsSync(dirPath)) {
+    const files = fs.readdirSync(dirPath);
+    return { files };
+  } else {
+    return { message: 'Directory does not exist' };
+  }
 };
 
 export default getAllFiles;
