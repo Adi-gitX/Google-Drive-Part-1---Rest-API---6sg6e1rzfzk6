0308a12f71bff66eef1c5e0a2275ffd55ef781d6
adi-gitX <adityakammati.workspace@gmail.com>
2024-09-04 15:59:39 +0530

    Update deleteAFile.js

--- tools/deleteAFile.js
+++ tools/deleteAFile.js
@@ -1,13 +1,11 @@
 import fs from 'fs';
-
 const deleteAFile = (fileName) => {
-  const filePath = `./root/${fileName}`;
-  if (fs.existsSync(filePath)) {
-    fs.unlinkSync(filePath);
-    return { message: 'File deleted successfully' };
-  } else {
-    return { message: 'File does not exist' };
-  }
+    try{
+        fs.unlinkSync(fileName);
+        return ('file deleted successfully')
+    }catch(err){
+        return err
+    }
 };
 
 export default deleteAFile;
